128f03c0783dd15bc7845a0eee25adeee360e640
SaidulGit <saidulhero4820@gmail.com>
2023-10-24 16:25:40 -0600

    Registation form complete

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,6 +1,69 @@
+import Swal from "sweetalert2";
+
 const Register = () => {
+ const handleRegister= e => {
+  e.preventDefault();
+const form = e.target;
+const name = form.name.value;
+const imgUrl = form.imageUrl.value;
+const email = form.email.value;
+const password = form.password.value;
+const user = {name,imgUrl,email,password};
+console.log(user);
+if (user){ Swal.fire('Successfully register!')}
+ }
+
+
+
   return (
-    <div>Register</div>
+    <div>
+  
+  <div>
+      <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Register now!</h1>
+    </div> <br />
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+
+      <form onSubmit={handleRegister} className="card-body">
+      <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input type="text" name="name" placeholder="Your Name" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Image URL</span>
+          </label>
+          <input type="text" name="imageUrl" placeholder="image url" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+          </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Register</button> <br />
+          <p>Allready have an account.Please <a className="text-base font-semibold text-blue-500" href="/login">Login </a></p>
+          
+        </div>
+      </form>
+     
+    </div>
+  </div>
+     </div>
+    </div>
+
+    </div>
   )
 }
 
