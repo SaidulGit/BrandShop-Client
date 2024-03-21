8d94ac8a51054b8b859608ed9c9b078a1e2b3aa2
SaidulGit <saidulhero4820@gmail.com>
2024-03-21 10:53:34 -0600

    make navbar sticky

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -21,16 +21,9 @@ const handleLogout = () => {
 
   return (
     <div>
-<div className="navbar bg-white">
+<div className="navbar bg-white fixed z-50">
   <div className="navbar-start">
     <div className="dropdown">
-      {/* <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label> */}
-      {/* <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li><a>Item 3</a></li>
-      </ul> */}
     </div>
    <Link to="/"> <img className="w-20 h-20 mr-2 rounded-2xl" src="https://i.ibb.co/9p8rVpG/make-a-logo-for-a-websitewebsite-name-Brand-sho.jpg" alt="" />
    </Link>
